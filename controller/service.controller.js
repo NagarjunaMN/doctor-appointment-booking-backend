681e0e41574843cd2b84c270e87d9950326e2a65
NagarjunaMN <nagmondru246@gmail.com>
2023-09-25 09:46:01 +0530

    added models

--- controller/service.controller.js
+++ controller/service.controller.js
@@ -1,10 +1,11 @@
 const  { StatusCodes }  = require('http-status-codes')
-
+const Service = require('../model/service.model')
 
 // get all service
 const getServices = async (req,res) => {
     try {
-        res.status(StatusCodes.OK).json({ msg: "get services"})
+        const data = await Service.find({})
+        res.status(StatusCodes.OK).json({ length: data.length, services: data })
     } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message })
     }
@@ -13,7 +14,11 @@ const getServices = async (req,res) => {
 // get single service
 const getSingleService = async (req,res) => {
     try {
-        res.status(StatusCodes.OK).json({ msg: "get single service"})
+        let id = req.params.id
+            let extService = await Service.findById({ _id: id })
+                if(!extService)
+                    return res.status(StatusCodes.NOT_FOUND).json({ msg: "Service id not found" })
+        res.status(StatusCodes.OK).json({ service: extService})
     } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message })
     }
